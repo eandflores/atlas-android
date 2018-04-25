5c5023a14f74ee83a0c1891ba1903a963af0e91f
Edgardo  Flores <edgardo@Edgardos-MacBook-Pro.local>
2018-04-25 18:52:47 -0300

    add layouts

--- app/src/main/kotlin/com/aim/atlas/ui/login/LoginActivity.kt
+++ app/src/main/kotlin/com/aim/atlas/ui/login/LoginActivity.kt
@@ -4,26 +4,21 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.Gravity
 import com.aim.atlas.R
 import com.aim.atlas.vo.User
-import android.widget.EditText
-import butterknife.BindView
+import com.aim.atlas.util.Constants
 import com.aim.atlas.vo.Resource
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_login.editTextEmail
+import kotlinx.android.synthetic.main.activity_login.editTextPassword
+import kotlinx.android.synthetic.main.activity_login.textViewHeaderLogin
+import kotlinx.android.synthetic.main.activity_login.buttonLogin
 
 
 class LoginActivity : AppCompatActivity() {
 
     private var viewModel: LoginViewModel? = null
-
-    @BindView(R.id.editTextEmail)
-    private val editTextEmail: EditText? = null
-
-    @BindView(R.id.editTextPassword)
-    private val editTextPassword: EditText? = null
-
-    @BindView(R.id.buttonLogin)
-    private val buttonLogin: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +26,21 @@ class LoginActivity : AppCompatActivity() {
 
         viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
 
+        setView()
         setEvents()
+    }
+
+    private fun setView() {
+        if(!intent.getBooleanExtra(Constants().expired_session, false)) {
+            textViewHeaderLogin.text = getString(R.string.login_header)
+            textViewHeaderLogin.gravity = Gravity.CENTER
+            textViewHeaderLogin.layoutParams = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f)
+        } else {
+            textViewHeaderLogin.text = getString(R.string.login_header_expired_session, "Edgardo")
+            textViewHeaderLogin.gravity = Gravity.START
+            textViewHeaderLogin.layoutParams = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.75f)
+            editTextEmail.setText("edgardo.flores.osben@gmail.com")
+        }
     }
 
     private fun setEvents() {
--- app/src/main/kotlin/com/aim/atlas/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/aim/atlas/ui/main/MainActivity.kt
@@ -6,6 +6,7 @@ import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import com.aim.atlas.R
 import com.aim.atlas.ui.login.LoginActivity
+import com.aim.atlas.util.Constants
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     fun goToLoginActivity() {
         val intent = Intent(this, LoginActivity::class.java)
+        intent.putExtra(Constants().expired_session, true)
         startActivity(intent)
         finish()
     }
